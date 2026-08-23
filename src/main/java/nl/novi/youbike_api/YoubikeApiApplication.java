195510d8f6e21b6f00558ab625198b3465709642
Withubyx <p-ost@live.com>
2026-08-23 21:45:13 +0200

    Initial commit

---
+++ src/main/java/nl/novi/youbike_api/YoubikeApiApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.youbike_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoubikeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoubikeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/youbike_api/YoubikeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.youbike_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoubikeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
